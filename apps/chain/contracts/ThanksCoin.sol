caf4bf67472b294ec77a26e95044d8e971878944
Ourai Lin <ourairyu@gmail.com>
2024-08-03 21:03:37 +0800

    feat(chain): add `ThanksCoin` and `People` smart contracts

---
+++ apps/chain/contracts/ThanksCoin.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.24;
+
+import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import { SelfGod } from "./SelfGod.sol";
+
+contract ThanksCoin is ERC20("Selfverse ThanksCoin", "TNKS"), SelfGod {
+  function _convertWithDecimals(uint256 amount) private view returns (uint256) {
+    return amount * 10 ** decimals();
+  }
+
+  function mint(address receiver, uint256 amount) external onlyOwner nonReentrant {
+    require(receiver != address(0), "Receiver must be specified.");
+    require(amount > 0, "Amount of $TNKS must be greater than 0.");
+
+    _mint(receiver, _convertWithDecimals(amount));
+  }
+}
