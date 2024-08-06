357596606a67a7e808110f9adb09e559b95c57b8
Ourai Lin <ourairyu@gmail.com>
2024-08-07 02:06:04 +0800

    feat(web): optimize `VisitorLayout`

--- apps/web/src/layouts/visitor/Visitor.tsx
+++ apps/web/src/layouts/visitor/Visitor.tsx
@@ -1,23 +1,36 @@
 import { Link, Outlet } from 'react-router-dom';
-import { Layout, Flex, Space } from 'antd';
+import { Layout, Flex, Space, Menu, type MenuProps } from 'antd';
 
 import Container from '../../components/container';
 import Passport from '../../components/passport';
 
+import style from './style.module.scss';
+
 function VisitorLayout() {
   const containerStyle = { maxWidth: 1280, margin: '0 auto' };
+  const menuItems: MenuProps['items'] = [
+    {
+      label: <Link to="/works">Works</Link>,
+      key: 'works',
+    },
+    {
+      label: <Link to="/donation">Donation</Link>,
+      key: 'donation',
+    },
+  ];
 
   return (
     <Container>
-      <Layout>
-        <Layout.Header style={{ backgroundColor: '#ccc' }}>
+      <Layout className={style.VisitorLayout}>
+        <Layout.Header className={style['VisitorLayout-header']}>
           <Flex align="center" justify="space-between" style={containerStyle}>
-            <div style={{ fontSize: 24 }}>
-              <Link to="/">Selfverse</Link>
-            </div>
             <Space>
-              <Link to="/works">Works</Link>
-              <Link to="/donation">Donation</Link>
+              <div style={{ fontSize: 24 }}>
+                <Link to="/">Selfverse</Link>
+              </div>
+              <Menu items={menuItems} mode="horizontal" />
+            </Space>
+            <Space>
               <Passport />
             </Space>
           </Flex>
