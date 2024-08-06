ae5cf77aac5acd89a3f2895a5b1675d9d4cbb1c6
Ourai Lin <ourairyu@gmail.com>
2024-08-07 01:43:11 +0800

    feat(web): make Hardhat customizable via `.env`

--- apps/web/src/components/container/Container.tsx
+++ apps/web/src/components/container/Container.tsx
@@ -1,23 +1,29 @@
 import type { PropsWithChildren } from 'react';
 import { ConfigProvider } from 'antd';
 import { WagmiWeb3ConfigProvider, MetaMask, OkxWallet, Sepolia, Hardhat } from '@ant-design/web3-wagmi';
+import type { Chain as AntdChain } from '@ant-design/web3';
+import type { Chain, Transport } from 'viem';
 import { createConfig, http } from 'wagmi';
 import { sepolia, hardhat } from 'wagmi/chains';
 
-const config = createConfig({
-  chains: [sepolia, hardhat],
-  transports: {
-    [sepolia.id]: http(),
-    [hardhat.id]: http()
-  }
-});
+const chainsProps: AntdChain[] = [Sepolia];
+const chains: [Chain, ...Chain[]] = [sepolia];
+const transports: Record<number, Transport> = { [sepolia.id]: http() };
+
+if (['true', true].indexOf(import.meta.env.VITE_HARDHAT_ENABLED) !== -1) {
+  chainsProps.push(Hardhat);
+  chains.push(hardhat);
+  transports[hardhat.id] = http();
+}
+
+const config = createConfig({ chains, transports });
 
 function Container(props: PropsWithChildren) {
   return (
     <ConfigProvider button={{ autoInsertSpace: false }}>
       <WagmiWeb3ConfigProvider
         config={config}
-        chains={[Sepolia, Hardhat]}
+        chains={chainsProps}
         wallets={[MetaMask(), OkxWallet()]}
         eip6963={{ autoAddInjectedWallets: true }}
       >
