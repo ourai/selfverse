cf2da95b16702f2da181e68295ec64363b818f22
Ourai Lin <ourairyu@gmail.com>
2024-08-09 21:05:45 +0800

    chore(chain): config for deploying to Sepolia testnet

--- apps/chain/hardhat.config.ts
+++ apps/chain/hardhat.config.ts
@@ -1,9 +1,18 @@
 import '@nomicfoundation/hardhat-toolbox';
 import type { HardhatUserConfig } from 'hardhat/config';
+import { config as confitDotEnv } from 'dotenv';
+
+confitDotEnv({ path: ['.env.local', '.env'] });
 
 const config: HardhatUserConfig = {
   defaultNetwork: 'localhost',
   solidity: '0.8.24',
+  networks: {
+    sepolia: {
+      url: process.env.TESTNET_SEPOLIA_ENDPOINT_URL || '',
+      accounts: [process.env.TESTNET_DEPLOY_ACCOUNT || ''],
+    },
+  },
 };
 
 export default config;
